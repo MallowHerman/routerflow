11eced643ba1dfbec6fbf7b71853ea76d8bb97d4
MallowHerman <hgoncalves2019@gmail.com>
2024-02-12 01:29:44 -0100

    [200~added user and telegram_notification_frequency fields to the UserProfile model in models.py.

--- src/routerflow/core/admin.py
+++ src/routerflow/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
--- src/routerflow/core/models.py
+++ src/routerflow/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from core.utils import BaseModel
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class UserProfile(BaseModel):
+    class TelegramNotificationFrequencyOptions(models.TextChoices):
+        INSTANT = "instant", "instant"
+        DAILY = "daily", "daily"
+        WEEKLY = "weekly", "weekly"
+        NEVER = "never", "never"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    telegram_notification_frequency = models.TextField(
+        max_length=16,
+        choices=TelegramNotificationFrequencyOptions.choices,
+        default=TelegramNotificationFrequencyOptions.INSTANT,
+    )
